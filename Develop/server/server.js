e0c3b37f5ae314b3a6c587d5e7b4ef733da2f4be
ChristopherGatt <chris.gatt.3@gmail.com>
2023-01-17 20:00:39 +1100

    readmet

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const path = require('path')
 const db = require('./config/connection')
 const routes = require('./routes')
+const { ApolloServer } = require('apollo-server-express')
+const { typeDefs, resolvers } = require('./schemas')
 
 const app = express()
 const PORT = process.env.PORT || 3001
@@ -16,18 +18,22 @@ app.use(express.json())
 
 // if we're in production, serve client/build as static assets
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')))
+}
+
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start()
   server.applyMiddleware({ app })
-
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')))
-  }
-
   app.use(routes)
 
   db.once('open', () => {
-    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`))
+    app.listen(PORT, () => {
+      console.log(`Now listening on localhost:${PORT}`)
+      console.log(
+        `use GraphQl at http://localhost:${PORT}${server.graphqlPath}`,
+      )
+    })
   })
 }
 
