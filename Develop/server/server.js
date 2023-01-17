91e5dead96c87e192d203c2b274045a1a5544cc3
ChristopherGatt <chris.gatt.3@gmail.com>
2023-01-17 19:39:26 +1100

    setup apollo

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -6,16 +6,29 @@ const routes = require('./routes')
 const app = express()
 const PORT = process.env.PORT || 3001
 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')))
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start()
+  server.applyMiddleware({ app })
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')))
+  }
+
+  app.use(routes)
+
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`))
+  })
 }
 
-app.use(routes)
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`))
-})
+startApolloServer(typeDefs, resolvers)
